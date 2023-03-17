84fdf8ca3e06942c9f05db8b1063c0c08e79895f
JNickBaughan <JNickBaughan@gmail.com>
2023-03-17 16:49:52 -0400

    basic variables

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,10 @@
 // goes to the target/
 
 fn main() {
-    println!("Hello, world!");
+    let num: i32 = 1;
+    let num_with_auto_type = 1;
+    let (even, odd) = (8, 9);
+    println!("explicit typed variable. num = {num}");
+    println!("auto typed variable. Compiler determines type - numWithAutoType = {num_with_auto_type}");
+    println!("multiple variables declared on one line. even = {even}, odd = {odd}");
 }
