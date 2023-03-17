cc88f4d77081091254f238a068ba19a2361eb688
JNickBaughan <JNickBaughan@gmail.com>
2023-03-17 16:55:04 -0400

    mutable variables

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,12 @@ fn main() {
     println!("explicit typed variable. num = {num}");
     println!("auto typed variable. Compiler determines type - numWithAutoType = {num_with_auto_type}");
     println!("multiple variables declared on one line. even = {even}, odd = {odd}");
+
+    // variables are immutable by default. For Safety, concurrency, and speed
+    // use the mut keyword to make variables that can be mutated 
+    let mut mut_num: i32 = 1;
+    println!("mut variables can be changed. mut_num = {mut_num}");
+    mut_num = 2;
+    println!("mut_num's value has changed = {mut_num}");
+
 }
