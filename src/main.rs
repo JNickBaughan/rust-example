c9d961364e03522d538a4ff03f086b069f4c53f8
JNickBaughan <JNickBaughan@gmail.com>
2023-03-17 16:35:08 -0400

    add notes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+//'cargo run' debug build, added to the target/debug folder
+//'cargo run --release' production build, added to the target/release folder
+// goes to the target/
+
 fn main() {
     println!("Hello, world!");
 }
